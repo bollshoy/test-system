c1817155e42a2b3cff406d20c618f7873116b531
bollshoy <b.orlov7709@gmail.com>
2024-10-26 13:58:22 +0300

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {Provider} from "react-redux";
 import store from "./redux/store.js";
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
